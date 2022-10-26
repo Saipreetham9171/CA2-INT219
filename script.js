b067e6cc8e009b91d151d0f0a727daf4a92f30d2
Saipreetham9171 <106610920+Saipreetham9171@users.noreply.github.com>
2022-10-26 10:11:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let navbar = document.getElementById("navbar");
+let navPos = navbar.getBoundingClientRect().top;
+
+
+window.addEventListener("scroll", (e) => {
+  let viewportHeight = window.innerHeight;
+  let scrollPos = window.scrollY;
+  if (scrollPos > navPos) {
+    navbar.classList.add("sticky");
+    header.classList.add("navbarOffsetMargin");
+  } else {
+    navbar.classList.remove("sticky");
+    header.classList.remove("navbarOffsetMargin");
+  }
+});
